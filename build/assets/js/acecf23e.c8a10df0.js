bb42a38069f52b3bc2a8f7af1ed6c9c574f5adb9
stone-bar <157951434+stone-bar@users.noreply.github.com>
2025-03-14 08:51:05 +0800

    change gitignore and config.ts

---
+++ build/assets/js/acecf23e.c8a10df0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkblog_03=self.webpackChunkblog_03||[]).push([[1903],{1912:l=>{l.exports=JSON.parse('{"blogBasePath":"/25blog-A/blog","blogTitle":"Blog","authorsListPath":"/25blog-A/blog/authors"}')}}]);
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Stone Nobody Note',
-  tagline: '便是人間好時節',
+  title: 'Stone Nobody Site',
+  tagline: '若無閒事掛心頭，便是人間好時節',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
